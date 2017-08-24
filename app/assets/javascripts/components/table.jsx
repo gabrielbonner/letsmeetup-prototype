ada2d98336bb67fffb59ffc697673e07928e29f0
Gabriel Bonner <gabrielbonner@gmail.com>
2017-08-23 22:53:53 -0700

    Toggle colors on click

--- app/assets/javascripts/components/table.jsx
+++ app/assets/javascripts/components/table.jsx
@@ -1,44 +1,64 @@
+class Row extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log('Row props =', props);
+    this.state = {};
+    this.state.color = "white";
+    this.toggleColor = this.toggleColor.bind(this);
+  }
+
+  toggleColor() {
+    const colors = {
+      white: "green",
+      green: "yellow",
+      yellow: "red",
+      red: "white",
+    }
+    this.setState({color: colors[this.state.color] });
+    console.log(this.state.color);
+  }
+
+  render() {
+      return <tr key={this.props.timeslot.id}>
+               <td>{this.props.timeslot.start}</td>
+               <td className={this.state.color} onClick={this.toggleColor}></td>
+               <td></td>
+               <td></td>
+               <td></td>
+               <td></td>
+            </tr>
+  }
+}
+
 class Table extends React.Component {
   constructor(props) {
     super(props);
-    console.log(props);
-    this.state = { color: "white" };
-    this.hello = this.hello.bind(this);
+    console.log('Table props =', props);
   }
-
-  hello() {
-    console.log(this);
-    this.setState({ color: "green" });
-    console.log('hello');
-  }
-
 
   render() {
+    var rows = [];
+    this.props.timeslots.forEach(function(timeslot) {
+        rows.push(<Row timeslot={timeslot} key={timeslot.id} />);
+      });
     return (
-      <table>
-        <thead>
-          <tr>
-            <th></th>
-            <th className="user">Fiona</th>
-            <th className="user">Gabriel</th>
-            <th className="user">Jeremy</th>
-            <th className="user">Kevin</th>
-            <th className="user">Kyle</th>
-          </tr>
-        </thead>
-        <tbody>
-          { this.props.timeslots.map((timeslot) => {
-            return <tr key={timeslot.id}>
-                     <td>{timeslot.start}</td>
-                     <td onClick={this.hello} className={this.state.color}></td>
-                     <td></td>
-                     <td></td>
-                     <td></td>
-                     <td></td>
-                  </tr>
-          })}
-        </tbody>
-      </table>
+      <div className="main">
+        <table>
+          <thead>
+            <tr>
+              <th></th>
+              <th className="user">Fiona</th>
+              <th className="user">Gabriel</th>
+              <th className="user">Jeremy</th>
+              <th className="user">Kevin</th>
+              <th className="user">Kyle</th>
+            </tr>
+          </thead>
+          <tbody>
+            {rows}
+          </tbody>
+        </table>
+      </div>
     );
   }
 }
