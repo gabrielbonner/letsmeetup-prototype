084425c5d7efa2459e559e774001f887bd4445b8
Gabriel Bonner <gabrielbonner@gmail.com>
2017-08-23 22:03:22 -0700

    Change color on click

--- app/assets/javascripts/components/table.jsx
+++ app/assets/javascripts/components/table.jsx
@@ -1,7 +1,17 @@
 class Table extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log(props);
+    this.state = { color: "white" };
+    this.hello = this.hello.bind(this);
+  }
+
   hello() {
+    console.log(this);
+    this.setState({ color: "green" });
     console.log('hello');
   }
+
 
   render() {
     return (
@@ -20,7 +30,7 @@ class Table extends React.Component {
           { this.props.timeslots.map((timeslot) => {
             return <tr key={timeslot.id}>
                      <td>{timeslot.start}</td>
-                     <td onClick={this.hello}></td>
+                     <td onClick={this.hello} className={this.state.color}></td>
                      <td></td>
                      <td></td>
                      <td></td>
